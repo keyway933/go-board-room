0e7386a7cab79c5ddc51161e5fe5d279728a7644
keyway933 <cba3516@gmail.com>
2026-07-13 04:45:01 +0800

    Allow local matchmaking from Pages

--- match-server.js
+++ match-server.js
@@ -16,6 +16,7 @@ function sendJson(res, status, data) {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Private-Network": "true",
     "Cache-Control": "no-store",
   });
   res.end(body);
